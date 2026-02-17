cdefd13ee7d77006c6cf185a72a9cbcb3a7ad713
adrenalinejunkie03 <muhammadsuhaib2805@gmail.com>
2026-02-17 15:25:25 +0500

    Mapping the values of stream to their double

--- stream_api.java
+++ stream_api.java
@@ -8,6 +8,11 @@ public class stream_api{
         List<Integer> num = Arrays.asList(4,5,6,7,9);
         Stream<Integer> data = num.stream();
 
+        // HERE I AM MAPPING THE NUMBERS IN THE STREAM TO THEIR DOUBLE VALUE, AND THEN PRINTING THEM OUT //
+        // MAP FUNCTION TAKES A FUNCTION AS AN ARGUMENT AND APPLIES IT TO EACH ELEMENT IN THE STREAM, RETURNING A NEW STREAM WITH THE MAPPED VALUES //
+        Stream<Integer> mappedData = data.map(n -> n*2);
+        mappedData.forEach(n -> System.out.println(n));
+
         // YOU CAN ONLY USE THE STREAM ONCE, AFTER THAT IT WILL BE CLOSED //
         // SO IF I RUN COUNT ON MY STREAM, I CANNOT USE IT AGAIN TO PRINT THE NUMBERS //
        // System.out.println(data.count());
